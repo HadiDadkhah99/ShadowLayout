04b778e332806c6024846d22c1689c018edeef10
hadi <hadidadkhah99@gmail.com>
2021-09-19 18:41:45 +0430

    hadi

--- app/src/main/java/com/foc/libs/shadowlayout/MainActivity.java
+++ app/src/main/java/com/foc/libs/shadowlayout/MainActivity.java
@@ -2,32 +2,11 @@ package com.foc.libs.shadowlayout;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.cardview.widget.CardView;
-import androidx.palette.graphics.Palette;
 
 import android.content.Context;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.BitmapShader;
 import android.graphics.Canvas;
-import android.graphics.Color;
-import android.graphics.LinearGradient;
-import android.graphics.Paint;
-import android.graphics.PorterDuff;
-import android.graphics.PorterDuffXfermode;
-import android.graphics.Shader;
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-import android.os.Handler;
-import android.util.Log;
-import android.view.View;
 import android.widget.FrameLayout;
-import android.widget.ImageView;
-import android.widget.Toast;
-
-import com.foc.libs.shadowLayoutPro.ShadowLayout;
-
-import java.util.concurrent.atomic.AtomicLong;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -39,15 +18,4 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    class Tes extends FrameLayout{
-
-        public Tes(@NonNull Context context) {
-            super(context);
-        }
-
-        @Override
-        protected void dispatchDraw(Canvas canvas) {
-            super.dispatchDraw(canvas);
-        }
-    }
 }
